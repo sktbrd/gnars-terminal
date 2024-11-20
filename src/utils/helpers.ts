97b6becf6ebe3ab9cd4596e2684effae1b099705
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-11-20 18:37:38 -0300

    refactor: add empty address control

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { zeroAddress } from 'viem';
+
 export function toObject(json?: object, space?: number) {
   if (!json) return json;
   return JSON.parse(
@@ -7,7 +9,7 @@ export function toObject(json?: object, space?: number) {
   );
 }
 
-export function formatEthAddress(address: string): string {
-  if (!address) return '';
+export function formatEthAddress(address?: string): string {
+  if (!address) address = zeroAddress;
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
 }
