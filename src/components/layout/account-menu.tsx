4f26b8c89a6b97b8138ed340895dc46e8b4f186d
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-02 19:24:08 -0300

    feat: add menu for account

---
+++ src/components/layout/account-menu.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  MenuContent,
+  MenuItem,
+  MenuItemCommand,
+  MenuRoot,
+  MenuTrigger,
+} from '@/components/ui/menu';
+import ConnectButton from './connect-button';
+import {
+  useAccount,
+  useBalance,
+  useConnect,
+  useEnsAvatar,
+  useEnsName,
+} from 'wagmi';
+import { useDisconnect } from 'wagmi';
+import { mainnet } from 'viem/chains';
+import { normalize } from 'viem/ens';
+import { Avatar } from '../ui/avatar';
+import { formatEthAddress } from '@/utils/helpers';
+import { LuLogOut, LuSparkle } from 'react-icons/lu';
+import { weiToSparks } from '@/utils/spark';
+import { Text } from '@chakra-ui/react';
+import { ColorModeIcon, useColorMode } from '../ui/color-mode';
+import Link from 'next/link';
+import { BsGithub } from 'react-icons/bs';
+
+export default function AccountMenu() {
+  const { toggleColorMode, colorMode } = useColorMode();
+  const { isConnected, address, isConnecting } = useAccount();
+  const { disconnect } = useDisconnect();
+
+  const { data: balance } = useBalance({
+    address,
+  });
+
+  const { data: ensName } = useEnsName({
+    address: address,
+    chainId: mainnet.id,
+  });
+
+  const { data: ensAvatar } = useEnsAvatar({
+    name: ensName ? normalize(ensName) : undefined,
+    chainId: mainnet.id,
+  });
+
+  if (!isConnected) {
+    return <ConnectButton />;
+  }
+
+  return (
+    <MenuRoot positioning={{ placement: 'bottom-end' }}>
+      <MenuTrigger asChild>
+        <Button size={'xs'} variant={'ghost'}>
+          {ensAvatar ? (
+            <Avatar variant={'subtle'} size='xs' w={5} h={5} src={ensAvatar} />
+          ) : null}
+          {ensName || formatEthAddress(address, 3)}
+        </Button>
+      </MenuTrigger>
+      <MenuContent>
+        <MenuItem value='balance' gap={1}>
+          <LuSparkle width={2} height={2} style={{ marginRight: '4px' }} />{' '}
+          Balance: {weiToSparks(balance?.value || 0n)}
+        </MenuItem>
+        <MenuItem value='color-mode' onClick={toggleColorMode}>
+          <ColorModeIcon />
+          Toogle Color Mode
+        </MenuItem>
+        <Link target='_blank' href='https://github.com/r4topunk/gnars-terminal'>
+          <MenuItem value='github'>
+            <BsGithub style={{ background: 'none' }} />
+            View Github
+          </MenuItem>
+        </Link>
+        <MenuItem
+          value='disconnect'
+          color='fg.error'
+          _hover={{ bg: 'bg.error', color: 'fg.error' }}
+          onClick={() => disconnect()}
+        >
+          <LuLogOut width={2} height={2} style={{ marginRight: '4px' }} />
+          Disconnect
+        </MenuItem>
+      </MenuContent>
+    </MenuRoot>
+  );
+}
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,30 +1,45 @@
 'use client';
 
 import { ColorModeButton } from '@/components/ui/color-mode';
-import { Heading, HStack, IconButton } from '@chakra-ui/react';
+import { Heading, HStack, IconButton, useMediaQuery } from '@chakra-ui/react';
 import Link from 'next/link';
 import { BsGithub } from 'react-icons/bs';
 import ConnectButton from './connect-button';
 import Sparks from './sparks';
+import AccountMenu from './account-menu';
 
 export default function Navbar() {
+  const [isLargerThanMd] = useMediaQuery(['(min-width: 768px)'], {
+    fallback: [true],
+  });
+
   return (
-    <HStack w={'full'} justify={'space-between'} p={2} colorPalette={'yellow'}>
+    <HStack
+      w={'full'}
+      justify={'space-between'}
+      px={{ base: 4, md: 2 }}
+      py={2}
+      colorPalette={'yellow'}
+    >
       <Link href={'/'}>
         <Heading size={{ base: '2xl', md: '4xl' }} as='h1'>
           Gnars Terminal
         </Heading>
       </Link>
-      <HStack>
-        <Link href='https://github.com/r4topunk/gnars-terminal'>
-          <IconButton variant={'ghost'} colorPalette={'black'} size={'sm'}>
-            <BsGithub style={{ background: 'none' }} />
-          </IconButton>
-        </Link>
-        <ColorModeButton variant={'ghost'} />
-        <ConnectButton />
-        <Sparks />
-      </HStack>
+      {isLargerThanMd ? (
+        <HStack>
+          <Link href='https://github.com/r4topunk/gnars-terminal'>
+            <IconButton variant={'ghost'} colorPalette={'black'} size={'sm'}>
+              <BsGithub style={{ background: 'none' }} />
+            </IconButton>
+          </Link>
+          <ColorModeButton variant={'ghost'} />
+          <ConnectButton />
+          <Sparks />
+        </HStack>
+      ) : (
+        <AccountMenu />
+      )}
     </HStack>
   );
 }
