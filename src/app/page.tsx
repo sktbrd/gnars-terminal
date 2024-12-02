91aab93b22bb62743d0d4dcf14c9804d96b4030b
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-02 17:34:30 -0300

    refactor: remove account card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import AccountCard from '@/components/cards/account';
 import AuctionCard from '@/components/cards/auction';
 import GovernorCard from '@/components/cards/governor';
-import Navbar from '@/components/layout/navbar';
 import { VStack } from '@chakra-ui/react';
 
 function App() {
   return (
     <VStack gap={4} align={'start'}>
-      <AccountCard />
       <AuctionCard />
       <GovernorCard />
     </VStack>
