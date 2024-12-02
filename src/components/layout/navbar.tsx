e0fb58755ffce0874415eb1ccf436f54e4aaa4fa
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-02 17:45:07 -0300

    feat: add link to header

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,17 +1,17 @@
 import { ColorModeButton } from '@/components/ui/color-mode';
-import { Heading, HStack, IconButton, VStack } from '@chakra-ui/react';
+import { Heading, HStack, IconButton } from '@chakra-ui/react';
 import Link from 'next/link';
 import { BsGithub } from 'react-icons/bs';
-import React from 'react';
-import { Avatar } from '../ui/avatar';
 import ConnectButton from './connect-button';
 
 export default function Navbar() {
   return (
     <HStack w={'full'} justify={'space-between'} p={2} colorPalette={'yellow'}>
-      <Heading size={{ base: '2xl', md: '4xl' }} as='h1'>
-        Gnars Terminal
-      </Heading>
+      <Link href={'/'}>
+        <Heading size={{ base: '2xl', md: '4xl' }} as='h1'>
+          Gnars Terminal
+        </Heading>
+      </Link>
       <HStack>
         <Link href='https://github.com/r4topunk/gnars-terminal'>
           <IconButton variant={'ghost'} colorPalette={'black'} size={'sm'}>
