f90096b13d885bcea9cf978acb92bd3103857af8
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-15 23:58:52 -0300

    feat: add flutuant navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,17 +1,35 @@
 'use client';
 
 import { ColorModeButton } from '@/components/ui/color-mode';
-import { Heading, HStack, IconButton, Image, Stack, Text, useMediaQuery } from '@chakra-ui/react';
-import Link from 'next/link';
+import {
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Stack,
+  Text,
+  Box,
+  VStack,
+  useMediaQuery,
+  Link,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
 import { BsGithub } from 'react-icons/bs';
+import { useState } from 'react';
 import ConnectButton from './connect-button';
 import Sparks from './sparks';
 import AccountMenu from './account-menu';
+import { Button } from '../ui/button';
+import { FcAbout } from 'react-icons/fc';
+import { LuHome } from 'react-icons/lu';
+import { MdOutlineHowToVote } from 'react-icons/md';
+import { IoDocumentTextOutline } from 'react-icons/io5';
 
 export default function Navbar() {
   const [isLargerThanMd] = useMediaQuery(['(min-width: 768px)'], {
     fallback: [true],
   });
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <HStack
@@ -20,26 +38,98 @@ export default function Navbar() {
       px={4}
       py={2}
       colorPalette={'yellow'}
+      position='relative'
     >
-      <Link href={'/'}>
-        <Stack direction='row' align='center' gap={2}>
-          <Image
-            src='https://gnars.com/images/logo.png'
-            alt='gnars-terminal'
-            boxSize={6}
-          />
-          <Heading size={{ base: 'xl', md: '2xl' }} as='h1' fontWeight={'bold'}>
-            TermGnar
-          </Heading>
-        </Stack>
-      </Link>
+      <Box
+        position='relative'
+        onMouseEnter={() => setShowMenu(true)}
+        onMouseLeave={() => setShowMenu(false)}
+      >
+        <NextLink href={'/'}>
+          <Stack direction='row' align='center' gap={2}>
+            <Image
+              src='https://gnars.com/images/logo.png'
+              alt='gnars-terminal'
+              boxSize={6}
+            />
+            <Heading
+              size={{ base: 'xl', md: '2xl' }}
+              as='h1'
+              fontWeight={'bold'}
+            >
+              TermGnar
+            </Heading>
+          </Stack>
+        </NextLink>
+
+        {showMenu && (
+          <VStack
+            position='absolute'
+            top='100%'
+            left='0'
+            pt={2}
+            gap={2}
+            zIndex={10}
+          >
+            <NextLink href='/' style={{ width: '100%' }}>
+              <Link
+                data-state='open'
+                _open={{
+                  animation: 'fade-in 300ms ease-out',
+                }}
+                fontSize={'xl'}
+                fontWeight={'medium'}
+              >
+                <LuHome />
+                Home
+              </Link>
+            </NextLink>
+            <NextLink href='/dao' style={{ width: '100%' }}>
+              <Link
+                fontSize={'xl'}
+                fontWeight={'medium'}
+                data-state='open'
+                _open={{
+                  animation: 'fade-in 600ms ease-out',
+                }}
+                _hidden={{
+                  animation: 'fade-out 600ms ease-in',
+                }}
+              >
+                <MdOutlineHowToVote />
+                DAO
+              </Link>
+            </NextLink>
+            <NextLink href='/about' style={{ width: '100%' }}>
+              <Link
+                fontSize={'xl'}
+                fontWeight={'medium'}
+                data-state='open'
+                _open={{
+                  animation: 'fade-in 900ms ease-out',
+                }}
+                _hidden={{
+                  animation: 'fade-out 300ms ease-in',
+                }}
+              >
+                <IoDocumentTextOutline />
+                About
+              </Link>
+              {/* <Text pb={1} _hover={{ textDecoration: 'underline' }} w='full'>
+                About
+              </Text> */}
+            </NextLink>
+          </VStack>
+        )}
+      </Box>
+
       {isLargerThanMd ? (
         <HStack>
-          <Link href='https://github.com/r4topunk/gnars-terminal'>
+          <NextLink href='https://github.com/r4topunk/gnars-terminal'>
             <IconButton variant={'ghost'} colorPalette={'black'} size={'sm'}>
               <BsGithub style={{ background: 'none' }} />
             </IconButton>
-          </Link>
+          </NextLink>
           <ColorModeButton variant={'ghost'} />
           <ConnectButton />
           <Sparks />
