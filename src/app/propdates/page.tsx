5cc823929949aa92d4d2999c49eaa3c94ed0d0bb
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2025-01-03 12:02:13 -0300

    feat: fetch proposals alongside propdates and display in content cards

--- src/app/propdates/page.tsx
+++ src/app/propdates/page.tsx
@@ -1,6 +1,8 @@
 import PropdatesContentCardList from '@/components/propdates/contentCard';
 import { fetchAllPropDates } from '@/services/supabase/propdates';
 import { Container, Heading, Text, VStack } from '@chakra-ui/react';
+import { fetchProposals } from '../services/proposal';
+import { DAO_ADDRESSES } from '@/utils/constants';
 
 export const metadata = {
   title: 'Propdates',
@@ -8,16 +10,30 @@ export const metadata = {
 };
 
 export default async function PropdatesPage() {
-  const { data } = await fetchAllPropDates();
-  console.log(data ? data[29] : null);
+  const [propdates, proposals] = await Promise.all([
+    fetchAllPropDates(),
+    fetchProposals(
+      DAO_ADDRESSES.token,
+      'proposalNumber',
+      'desc',
+      1000,
+      {},
+      false,
+      true
+    ),
+  ]);
+
   return (
     <Container p={0} maxW='breakpoint-lg'>
       <VStack gap={{ base: 2, md: 4 }} align={'start'}>
         <Heading size={{ base: '2xl', md: '4xl' }} as='h1'>
           Propdates
         </Heading>
-        {data?.length ? (
-          <PropdatesContentCardList propdates={data} />
+        {propdates?.data?.length ? (
+          <PropdatesContentCardList
+            propdates={propdates.data}
+            proposals={proposals}
+          />
         ) : (
           <Text>No propdates yet</Text>
         )}
