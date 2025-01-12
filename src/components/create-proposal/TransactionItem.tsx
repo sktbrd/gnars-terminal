2bf23227e4ba821fc64c7459d4bb3f7ba1138037
sktbrd <sktbrd.eth@gmail.com>
2025-01-12 20:23:23 -0300

    feat: enhance TransactionItem component with token balance display and amount handling

--- src/components/create-proposal/TransactionItem.tsx
+++ src/components/create-proposal/TransactionItem.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React, { useState } from "react";
-import { VStack, Text, HStack, SimpleGrid, Button } from "@chakra-ui/react";
+import { VStack, Text, HStack, SimpleGrid, Button, Image, Badge } from "@chakra-ui/react";
 import { Radio, RadioGroup } from "@/components/ui/radio"
 import TransactionForm from "./TransactionForm";
 import { SENDIT_CONTRACT_ADDRESS, USDC_CONTRACT_ADDRESS } from "@/utils/constants";
 import { Address, isAddress, parseEther, parseUnits } from 'viem';
 import { LuChevronDown } from "react-icons/lu";
 import { prepareTransactionData, formatTransactionDetails } from '@/utils/transactionUtils';
+import useTreasure from "@/hooks/useTreasure";
 
 type TransactionItemProps = {
     type: string;
@@ -22,22 +23,25 @@ const TransactionItem: React.FC<TransactionItemProps> = ({ type, onAdd, onCancel
     const [file, setFile] = useState<File | null>(null);
     const [editionType, setEditionType] = useState<string>("Fixed");
     const [simulationResult, setSimulationResult] = useState<"success" | "fail" | "pending" | null>(null);
-    // const [transactionAdded, setTransactionAdded] = useState<boolean>(false); // Track if transaction is added
+    const [amount, setAmount] = useState<number>(0); // Add state for amount
+
+    const treasuryAddress = process.env.NEXT_PUBLIC_TREASURY as Address || '0x';
+    const { tokens, usdcBalance, ethBalance, senditBalance, gnarsNftBalance, isLoading } = useTreasure(treasuryAddress);
 
     const getTokenDetails = (type: string) => {
         switch (type) {
             case "SEND ETH":
-                return { tokenAddress: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", decimals: 18 };
+                return { tokenAddress: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", name: "ETH", decimals: 18, image: "/images/ethereum.png" };
             case "SEND USDC":
-                return { tokenAddress: USDC_CONTRACT_ADDRESS, decimals: 6 };
+                return { tokenAddress: USDC_CONTRACT_ADDRESS, name: "USDC", decimals: 6, image: "/images/usdc.png" };
             case "SEND IT":
-                return { tokenAddress: SENDIT_CONTRACT_ADDRESS, decimals: 18 };
+                return { tokenAddress: SENDIT_CONTRACT_ADDRESS, name: "SENDIT", decimals: 18, image: "/images/sendit.jpg" };
             default:
-                return { tokenAddress: "", decimals: 18 };
+                return { tokenAddress: "", name: "", decimals: 18, image: "" };
         }
     };
 
-    const { tokenAddress, decimals } = getTokenDetails(type);
+    const { tokenAddress, name, decimals, image } = getTokenDetails(type);
     const UNLIMITED_EDITION_SIZE = "18446744073709551615";
 
     const fields = (() => {
@@ -45,16 +49,11 @@ const TransactionItem: React.FC<TransactionItemProps> = ({ type, onAdd, onCancel
             case "SEND ETH":
             case "SEND USDC":
             case "SEND IT":
+            case "SEND NFT":
                 return [
                     { name: "amount", placeholder: "Enter amount", validate: (value: string) => !isNaN(Number(value)) || "Invalid amount." },
                     { name: "toAddress", placeholder: "Enter destination address", validate: (value: string) => isAddress(value) || "Invalid Ethereum address." }
                 ];
-            case "SEND NFT":
-                return [
-                    { name: "tokenId", placeholder: "Enter token ID", validate: (value: string) => !isNaN(Number(value)) || "Invalid token ID." },
-                    { name: "toAddress", placeholder: "Enter destination address", validate: (value: string) => isAddress(value) || "Invalid Ethereum address." }
-                ];
-            //NEEDS TO APPROVE TREASURE AS A MINTER OF NFT CONTRACT BEFORE THE DAO USE THIS ONE
             case "AIRDROP RANDOM GNAR":
                 return [
                     { name: "amount", placeholder: "Amount", validate: (value: string) => !isNaN(Number(value)) || "Invalid amount." },
@@ -108,6 +107,18 @@ const TransactionItem: React.FC<TransactionItemProps> = ({ type, onAdd, onCancel
         onAdd({ type: `${transaction.type}`, details });
     };
 
+    const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseFloat(event.target.value); // Parse to float for decimal handling
+        console.log("Input Amount:", value); // Debugging
+        setAmount(isNaN(value) ? 0 : value); // Default to 0 if invalid
+    };
+
+    const usdcBalanceNumber = parseFloat(usdcBalance?.toString() || "0");
+    const ethBalanceNumber = ethBalance; // Use the exact value for Ethereum balance
+    const senditBalanceNumber = parseFloat(senditBalance?.toString() || "0");
+
+    console.log("usdcBalance:", usdcBalanceNumber, "ethBalance:", ethBalanceNumber, "senditBalance:", senditBalanceNumber, "amount:", amount); // Debug log
+
     return (
         <VStack gap={4} align="stretch" p={4} borderWidth="1px" borderRadius="md">
             <HStack justifyContent="space-between">
@@ -116,6 +127,37 @@ const TransactionItem: React.FC<TransactionItemProps> = ({ type, onAdd, onCancel
                 </Text>
                 <LuChevronDown />
             </HStack>
+            {(type === "SEND ETH" || type === "SEND USDC" || type === "SEND IT" || type === "SEND NFT") && isLoading ? (
+                <Text>Loading...</Text>
+            ) : (
+                <>
+                    {type === "SEND ETH" && (
+                        <HStack>
+                            <Image src={image} alt={name} boxSize="20px" />
+
+                            <>Balance:<Badge colorScheme="green">{ethBalanceNumber.toFixed(7)} {name}</Badge></>
+                        </HStack>
+                    )}
+                    {type === "SEND USDC" && (
+                        <HStack>
+                            <Image src={image} alt={name} boxSize="20px" />
+                            <Text>Balance: {usdcBalanceNumber.toFixed(3)} {name}</Text>
+                        </HStack>
+                    )}
+                    {type === "SEND IT" && (
+                        <HStack>
+                            <Image src={image} alt={name} boxSize="20px" />
+                            <Text>Balance: {senditBalanceNumber.toFixed(3)} {name}</Text>
+                        </HStack>
+                    )}
+                    {type === "SEND NFT" && (
+                        <HStack>
+                            <Image src="https://gnars.com/images/logo.png" alt="Gnars" boxSize="20px" />
+                            <Text>Balance: {gnarsNftBalance} GNARS</Text>
+                        </HStack>
+                    )}
+                </>
+            )}
             {type === "DROPOSAL MINT" && (
                 <RadioGroup
                     value={editionType}
@@ -131,7 +173,7 @@ const TransactionItem: React.FC<TransactionItemProps> = ({ type, onAdd, onCancel
             )}
             <TransactionForm
                 type={type}
-                fields={fields}
+                fields={fields.map(field => field.name === "amount" ? { ...field, onChange: handleAmountChange } : field)} // Add onChange handler to amount field
                 onAdd={handleAdd}
                 onCancel={onCancel}
                 onFileChange={handleFileChange}
