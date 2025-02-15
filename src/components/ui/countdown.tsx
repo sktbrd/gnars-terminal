b49045d14cf478385ce8eee193abd021729d8060
r4topunk <gustavo-kuhl@hotmail.com>
2025-02-15 12:26:16 -0300

    feat: add QueueProposal component with countdown timer for proposal expiration

---
+++ src/components/ui/countdown.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Text } from '@chakra-ui/react';
+import { default as ReactCountDown, zeroPad } from 'react-countdown';
+
+interface CountdownRendererProps {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const CountdownRenderer = ({
+  days,
+  hours,
+  minutes,
+  seconds,
+}: CountdownRendererProps) => (
+  <Text display={'inline-block'}>
+    {days >= 0 && `${zeroPad(days)}d `}
+    {hours >= 0 && `${zeroPad(hours)}h `}
+    {minutes >= 0 && `${zeroPad(minutes)}m `}
+    {zeroPad(seconds)}s
+  </Text>
+);
+
+export function Countdown({ date }: { date: number }) {
+  return <ReactCountDown renderer={CountdownRenderer} date={date} />;
+}
