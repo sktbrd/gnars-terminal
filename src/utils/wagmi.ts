41b8ac66a53dfdf20a7e5a1a3e11ebb23b3f919d
r4topunk <gustavo-kuhl@hotmail.com>
2025-03-03 10:08:14 -0300

    refactor: simplify wagmi configuration by removing unused imports and mainnet chain

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -1,18 +1,14 @@
 'use client';
 
-import { cookieStorage, createStorage } from 'wagmi';
-import { base, mainnet } from 'wagmi/chains';
+import { base } from 'wagmi/chains';
 import { WC_PROJECT_ID } from './constants';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
 export function getConfig() {
   return getDefaultConfig({
     appName: 'Gnars',
-    chains: [base, mainnet],
+    chains: [base],
     projectId: WC_PROJECT_ID,
-    storage: createStorage({
-      storage: cookieStorage,
-    }),
     ssr: true,
   });
 }
