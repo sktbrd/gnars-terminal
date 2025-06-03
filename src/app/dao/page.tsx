904c6871d62c8731bec11091aa1d1d45b00832aa
r4to <r4topunk.eth@gmail.com>
2025-06-03 17:55:49 -0300

    ✨ feat: add ContractsCard component to display DAO contracts
    Introduced ContractsCard to showcase smart contracts deployed on the
    Base network, enhancing the DAO page with contract details and
    interactivity for users.

--- src/app/dao/page.tsx
+++ src/app/dao/page.tsx
@@ -1,5 +1,6 @@
 import GovernorCard from '@/components/cards/governor';
 import MembersCard from '@/components/cards/members';
+import ContractsCard from '@/components/cards/contracts';
 import { Container, Heading, VStack, Tabs } from '@chakra-ui/react';
 export const revalidate = 0;
 
@@ -13,13 +14,17 @@ function DaoPage() {
         <Tabs.Root defaultValue='proposals' w='full'>
           <Tabs.List w='full' justifyContent='center'>
             <Tabs.Trigger value='proposals'>Proposals</Tabs.Trigger>
-            <Tabs.Trigger value='placeholder'>Members</Tabs.Trigger>
+            <Tabs.Trigger value='members'>Members</Tabs.Trigger>
+            <Tabs.Trigger value='contracts'>Contracts</Tabs.Trigger>
           </Tabs.List>
           <Tabs.Content value='proposals' w='full'>
             <GovernorCard />
           </Tabs.Content>
-          <Tabs.Content value='placeholder' w='full'>
+          <Tabs.Content value='members' w='full'>
             <MembersCard />
+          </Tabs.Content>
+          <Tabs.Content value='contracts' w='full'>
+            <ContractsCard />
           </Tabs.Content>
         </Tabs.Root>
       </VStack>
